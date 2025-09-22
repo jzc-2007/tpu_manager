a38fefa1c1fe2ddcae01f38827e214ea5b2c9b9f
sqa-abroad <jzc_2007@mit.edu>
2025-09-22 16:35:27 +0000

    xibo update v5 check env

--- batch_run.sh
+++ batch_run.sh
@@ -1,4 +1,5 @@
-# python /home/jzc/zhichengjiang/working/xibo_tpu_manager/tpu.py applyy sy4 &
-python /home/jzc/zhichengjiang/working/xibo_tpu_manager/tpu.py applyy sy7 &
-python /home/jzc/zhichengjiang/working/xibo_tpu_manager/tpu.py applyy sy10 &
-python /home/jzc/zhichengjiang/working/xibo_tpu_manager/tpu.py applyy sy18 &
+# python /home/jzc/zhichengjiang/working/xibo_tpu_manager/tpu.py applyy v4-16-sy2 &
+# python /home/jzc/zhichengjiang/working/xibo_tpu_manager/tpu.py applyy v4-16-sy3 &
+# python /home/jzc/zhichengjiang/working/xibo_tpu_manager/tpu.py applyy v4-16-sy4 &
+# python /home/jzc/zhichengjiang/working/xibo_tpu_manager/tpu.py applyy v4-16-sy5 &
+# python /home/jzc/zhichengjiang/working/xibo_tpu_manager/tpu.py applyy v4-16-sy6 &
